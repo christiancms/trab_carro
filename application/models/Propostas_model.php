b5540d6662dc552c3ce54da83a54a5d6f81afc35
Maicon <infobyte@infobyteinformatica.com>
2016-10-08 14:56:26 -0300

    Model, Controler, View da Proposta

---
+++ application/models/Propostas_model.php
@@ -0,0 +1,8 @@
+<?php
+
+class Propostas_model extends CI_Model {
+    
+  
+        
+    }
+
---
+++ application/views/propostas_view.php
@@ -0,0 +1,29 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-10"><h4>Revenda Carros - Propostas Veículos</h4></div>
+        
+
+
+
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Modelo do Veículo</th>
+                    <th>Marca</th>
+                    <th>Preço</th>
+                    <th>Cor</th>
+                    <th>Ano</th>
+                    <th>Proposta</th>
+                    <th>Foto</th>
+                    <th>Ações</th>
+
+
+                </tr>
+            </thead>
+           
+        </table>    
+    </div>
+</body>
+</html>
