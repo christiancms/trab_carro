e206af86e2b56306516bf2d7abff76bf63812dc4
christiancms <christian.souza@gmail.com>
2016-10-08 11:59:54 -0300

    Novo visual para o destaque

--- application/views/home.php
+++ application/views/home.php
@@ -2,23 +2,17 @@
     <h3>Revenda de Carros</h3>
     <p>O veículo que você procura, está Aqui.</p>
 </div>
-
 <?php foreach ($veiculos as $veiculo) { ?>
-
-    <?= $veiculo->id ?>
-    <?= $veiculo->modelo ?>
-    <?= $veiculo->marca ?>
-    <?= number_format($veiculo->preco, 2, ',', '.') ?>
-    <?= $veiculo->cor ?>
-    <?= $veiculo->ano ?>
-    <?= date_format(date_create($veiculo->datacad), 'd/m/Y') ?>
-    <img src="./fotos/<?= $veiculo->foto ?>" width=120 height=80>
-    Teste
-
-
+    <div class="col-sm-4" style="text-align:center">
+        <h3><?= $veiculo->marca ?>-<?= $veiculo->modelo ?></h3>
+        <img src="./fotos/<?= $veiculo->foto ?>" style="width: 120px; height: 80px;">
+        <h5>R$ <?= number_format($veiculo->preco, 2, ',', '.') ?></h5>
+        <h6>Ano: <?= $veiculo->ano ?></h6>
+        <div class="w3-section">
+            <button class="w3-btn w3-green">Proposta</button>
+        </div>
+    </div>
 <?php } ?>
-
-<div class="col-sm-12"><p class="footer">Página renderizada em <strong>{elapsed_time}</strong> segundos.</p></div>
 </body>
 </html>
 
